f067561812e5ecc60d46155fa06d48bc633ec84b
AJ Schmidt <ajs4392@gmail.com>
2020-07-19 14:21:54 -0400

    add more tests

--- tests/components/alarmdecoder/test_config_flow.py
+++ tests/components/alarmdecoder/test_config_flow.py
@@ -1,4 +1,4 @@
-"""Test the NEW_NAME config flow."""
+"""Test the AlarmDecoder config flow."""
 from alarmdecoder.util import NoDeviceError
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.alarmdecoder.const import (
@@ -7,6 +7,13 @@ from homeassistant.components.alarmdecoder.const import (
     CONF_CODE_ARM_REQUIRED,
     CONF_DEVICE_BAUD,
     CONF_DEVICE_PATH,
+    CONF_RELAY_ADDR,
+    CONF_RELAY_CHAN,
+    CONF_ZONE_LOOP,
+    CONF_ZONE_NAME,
+    CONF_ZONE_NUMBER,
+    CONF_ZONE_RFID,
+    CONF_ZONE_TYPE,
     DOMAIN,
     DEFAULT_ARM_OPTIONS,
     DEFAULT_ZONE_OPTIONS,
@@ -14,15 +21,11 @@ from homeassistant.components.alarmdecoder.const import (
     OPTIONS_ZONES,
     PROTOCOL_SERIAL,
     PROTOCOL_SOCKET,
-    CONF_ZONE_LOOP,
-    CONF_ZONE_NAME,
-    CONF_ZONE_NUMBER,
-    CONF_ZONE_RFID,
-    CONF_ZONE_TYPE,
 )
+
+from homeassistant.components.binary_sensor import DEVICE_CLASS_WINDOW
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_PROTOCOL
 from homeassistant.core import HomeAssistant
-
 
 from tests.async_mock import patch
 from tests.common import MockConfigEntry
@@ -152,7 +155,7 @@ async def test_setup_connection_error(hass: HomeAssistant):
         assert result["errors"] == {"base": "service_unavailable"}
 
 
-async def test_options_arm_flow(hass):
+async def test_options_arm_flow(hass: HomeAssistant):
     """Test arm options flow."""
     user_input = {
         CONF_ALT_NIGHT_MODE: True,
@@ -188,16 +191,10 @@ async def test_options_arm_flow(hass):
     }
 
 
-async def test_options_zone_flow(hass):
-    """Test zone options flow."""
+async def test_options_zone_flow(hass: HomeAssistant):
+    """Test options flow for adding/deleting zones."""
     zone_number = "2"
-
-    # Check what happens when zone fields are left empty. Are they included in the final dict?
-    zone_settings = {
-        CONF_ZONE_NAME: True,
-        CONF_AUTO_BYPASS: True,
-        CONF_CODE_ARM_REQUIRED: True,
-    }
+    zone_settings = {CONF_ZONE_NAME: "Front Entry", CONF_ZONE_TYPE: DEVICE_CLASS_WINDOW}
     entry = MockConfigEntry(domain=DOMAIN)
     entry.add_to_hass(hass)
 
@@ -211,17 +208,175 @@ async def test_options_zone_flow(hass):
     )
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "arm_settings"
+    assert result["step_id"] == "zone_select"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={CONF_ZONE_NUMBER: zone_number},
+    )
 
     with patch(
         "homeassistant.components.alarmdecoder.async_setup_entry", return_value=True
     ):
         result = await hass.config_entries.options.async_configure(
-            result["flow_id"], user_input=user_input,
+            result["flow_id"], user_input=zone_settings,
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert entry.options == {
+        OPTIONS_ARM: DEFAULT_ARM_OPTIONS,
+        OPTIONS_ZONES: {zone_number: zone_settings},
+    }
+
+    # Make sure zone can be removed...
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "init"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={"edit_selection": "Zones"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_select"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={CONF_ZONE_NUMBER: zone_number},
+    )
+
+    with patch(
+        "homeassistant.components.alarmdecoder.async_setup_entry", return_value=True
+    ):
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"], user_input={},
         )
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert entry.options == {
         OPTIONS_ARM: DEFAULT_ARM_OPTIONS,
         OPTIONS_ZONES: {},
+    }
+
+
+async def test_options_zone_flow_validation(hass: HomeAssistant):
+    """Test input validation for zone options flow."""
+    zone_number = "2"
+    zone_settings = {CONF_ZONE_NAME: "Front Entry", CONF_ZONE_TYPE: DEVICE_CLASS_WINDOW}
+    entry = MockConfigEntry(domain=DOMAIN)
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "init"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={"edit_selection": "Zones"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_select"
+
+    # Zone Number must be int
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={CONF_ZONE_NUMBER: "asd"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_select"
+    assert result["errors"] == {CONF_ZONE_NUMBER: "int"}
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={CONF_ZONE_NUMBER: zone_number},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+
+    # CONF_RELAY_ADDR & CONF_RELAY_CHAN are inclusive
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={**zone_settings, CONF_RELAY_ADDR: "1"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+    assert result["errors"] == {"base": "relay_inclusive"}
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={**zone_settings, CONF_RELAY_CHAN: "1"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+    assert result["errors"] == {"base": "relay_inclusive"}
+
+    # CONF_RELAY_ADDR, CONF_RELAY_CHAN must be int
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={**zone_settings, CONF_RELAY_ADDR: "abc", CONF_RELAY_CHAN: "abc"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+    assert result["errors"] == {
+        CONF_RELAY_ADDR: "int",
+        CONF_RELAY_CHAN: "int",
+    }
+
+    # CONF_ZONE_LOOP depends on CONF_ZONE_RFID
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={**zone_settings, CONF_ZONE_LOOP: "1"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+    assert result["errors"] == {CONF_ZONE_LOOP: "loop_rfid"}
+
+    # CONF_ZONE_LOOP must be int
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={**zone_settings, CONF_ZONE_RFID: "rfid123", CONF_ZONE_LOOP: "ab"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+    assert result["errors"] == {CONF_ZONE_LOOP: "int"}
+
+    # CONF_ZONE_LOOP must be between [1,4]
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={**zone_settings, CONF_ZONE_RFID: "rfid123", CONF_ZONE_LOOP: "5"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "zone_details"
+    assert result["errors"] == {CONF_ZONE_LOOP: "loop_range"}
+
+    # All valid settings
+    with patch(
+        "homeassistant.components.alarmdecoder.async_setup_entry", return_value=True
+    ):
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                **zone_settings,
+                CONF_ZONE_RFID: "rfid123",
+                CONF_ZONE_LOOP: "2",
+                CONF_RELAY_ADDR: "12",
+                CONF_RELAY_CHAN: "1",
+            },
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert entry.options == {
+        OPTIONS_ARM: DEFAULT_ARM_OPTIONS,
+        OPTIONS_ZONES: {
+            zone_number: {
+                **zone_settings,
+                CONF_ZONE_RFID: "rfid123",
+                CONF_ZONE_LOOP: "2",
+                CONF_RELAY_ADDR: "12",
+                CONF_RELAY_CHAN: "1",
+            }
+        },
     }
